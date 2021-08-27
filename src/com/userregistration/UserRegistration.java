4ced559bd74a8a77719043c183fe5d2c498b465b
devesh1806 <42887196+devesh1806@users.noreply.github.com>
2021-08-27 21:19:42 +0530

    [Devesh]: Added Lastname validation feature

--- src/com/userregistration/UserRegistration.java
+++ src/com/userregistration/UserRegistration.java
@@ -5,8 +5,24 @@ public class UserRegistration {
 	public static void main(String[] args) {
 		String regex = "^[A-Z][a-z]{3,}";
 		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		if (str.matches(regex)) System.out.println("Firstname is valid.");
-		else System.out.println("Firstname is not valid.");
+		System.out.println("Validate 1.Firstname 2.Lastname");
+		int option = sc.nextInt();
+		switch(option) {
+			case 1:
+				String str = sc.nextLine();
+				if (str.replace(" ", "").matches(regex)) System.out.println("Firstname is valid.");
+				else System.out.println("Firstname is not valid.");
+				break;
+			
+			case 2:
+				String str1 = sc.nextLine();
+				if (str1.replace(" ", "").matches(regex)) System.out.println("Lastname is valid.");
+				else System.out.println("Lastname is not valid.");
+				break;
+				
+			default:
+				break;
+		}
+		
 	}
 }
