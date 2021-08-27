a64de61a4e87728aa38fe616c74ea74a565f7ffd
devesh1806 <42887196+devesh1806@users.noreply.github.com>
2021-08-27 21:12:12 +0530

    [Devesh]: Added Firstname validation feature

--- src/com/userregistration/UserRegistration.java
+++ src/com/userregistration/UserRegistration.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 
 public class UserRegistration {
 	public static void main(String[] args) {
-		
+		String regex = "^[A-Z][a-z]{3,}";
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		if (str.matches(regex)) System.out.println("Firstname is valid.");
+		else System.out.println("Firstname is not valid.");
 	}
 }
